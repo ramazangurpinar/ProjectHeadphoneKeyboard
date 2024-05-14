92a1a1048efe24907449ca2e0852f6e1a2dc856b
Albert Pardo <albert@journi.uk>
2024-05-14 10:56:39 +0200

    First commit

---
+++ headphones-market.core.Api/Model/Headphone.cs
@@ -0,0 +1,18 @@
+﻿namespace headphones_market.core.Api.Model;
+
+public class Headphone
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public string Description { get; set; }
+    public string Color { get; set; }
+    public double Price { get; set; }
+    public string ImageFileName { get; set; }
+    public string Type { get; set; }
+    public bool Wireless { get; set; }
+    public string BatteryLife { get; set; }
+    public string NoiseCancellationType { get; set; }
+    public string Weight { get; set; }
+    public bool Mic { get; set; }
+}
